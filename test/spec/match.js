c08f701f22ac8ce733836b3d77752bbb790027bb
cyberfunk <hello@cyberfunk.org>
2014-10-07 22:54:58 +0800

    Assert on process pattern match.

---
+++ test/spec/match.js
@@ -0,0 +1,26 @@
+var expect = require('chai').expect
+  , mock = require('../util/mock')
+  , pkg = require('../../package.json')
+  , program = require('../../lib/die');
+
+describe('cli-die:', function() {
+
+  before(mock.setup);
+  after(mock.teardown);
+
+  beforeEach(mock.before);
+
+  it('should match process by pattern', function(done) {
+    var args = mock.args(['m', '/mock-/']);
+    var def = program(pkg, mock.name);
+    def.program.on('complete', function(req) {
+      mock.after();
+      expect(req.match).to.be.an('object');
+      var keys = Object.keys(req.match);
+      expect(keys.length).to.eql(mock.processes.length);
+      expect(keys).to.eql(mock.pids);
+      done();
+    })
+    def.parse(args);
+  });
+})
--- test/util/mock.js
+++ test/util/mock.js
@@ -1,7 +1,20 @@
-var writer;
+var path = require('path')
+  , fs = require('fs')
+  , spawn = require('child_process').spawn
+  , bin = path.normalize(path.join(__dirname, '..', 'bin'))
+  , processes = []
+  , pids = []
+  , exes = fs.readdirSync(bin).map(function(f){return path.join(bin, f)})
+  , args = [
+    ['--node-version', '--semver'],
+    ['-xvf'],
+  ]
+  , writer;
 
 var mock = {
   name: 'die',
+  processes: processes,
+  pids: pids,
 }
 
 mock.before = function(done) {
@@ -13,6 +26,37 @@ mock.before = function(done) {
 mock.after = function(done) {
   if(writer) process.stdout.write = writer;
   if(done) done();
+}
+
+mock.setup = function(done) {
+  var opts = {};
+  args.forEach(function(argv) {
+    exes.forEach(function(cmd) {
+      processes.push(spawn(cmd, argv, opts));
+    })
+  })
+
+  mock.pids = processes.map(function(ps) {
+    return '' + ps.pid;
+  })
+
+  function cb() {
+    return done();
+    //require('child_process').exec('ps', function(e, so, se) {
+      //console.dir(so.split('\n'));
+      //done();
+    //})
+  }
+
+  // time to spin up
+  setTimeout(cb, 250);
+}
+
+mock.teardown = function(done) {
+  processes.forEach(function(ps) {
+    process.kill(ps.pid);
+  })
+  done();
 }
 
 mock.args = function(argv) {
